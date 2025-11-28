9e805e5d3fe3dc4f8b6aa7fd30a916bb1cd16a56
Wanjirrru <melissawanjiru@yahoo.com>
2025-11-29 00:19:58 +0300

    Minor changes to _config.js & server.js to ensure building

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://melissawanjiru_db_user:nZwhmi6vU2GjfofU@1p1-cluster.ingxmug.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://melissawanjiru_db_user:nZwhmi6vU2GjfofU@1p1-cluster.ingxmug.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://melissawanjiru_db_user:nZwhmi6vU2GjfofU@1p1-cluster.ingxmug.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
+
 module.exports = config;
